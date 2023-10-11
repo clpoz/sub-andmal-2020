a4ddb305b043eb254030a9e227b0ab93d7e99beb
clpoz <72326693+fengshou-chen@users.noreply.github.com>
2023-10-11 15:24:22 +0800

    Update models2.py

--- models2.py
+++ models2.py
@@ -9,48 +9,7 @@ import torch.optim as optim
 from torchvision import models
 import torch.nn.functional as F
 
-class Model1(torch.nn.Module):
-    def __init__(self,in_features,out_features,in_channels=1,out_channels=128):
-        super(Model1, self).__init__()
-        self.modelName = 'model1'
-        self.relu = torch.nn.ReLU()
-        self.l1 = torch.nn.Linear(in_features=in_features,out_features=256)
-        self.l2 = torch.nn.Linear(in_features=256,out_features=128)
-        self.l3 = torch.nn.Linear(in_features=128,out_features=out_features)
-        self.bn1d1 = torch.nn.BatchNorm1d(in_features)
-        self.bn1d2 = torch.nn.BatchNorm1d(256)
-        self.bn1d3 = torch.nn.BatchNorm1d(128)
 
-    def forward(self,x):
-        x = self.relu(self.l1(x))
-        x = self.relu(self.l2(x))
-        x = self.l3(x)
-
-        return x
-
-
-class Model2(torch.nn.Module):
-    def __init__(self,in_features=9503,out_features=1,in_channels=1,out_channels=128):
-        super(Model2, self).__init__()
-        self.modelName = '2model2'
-        self.relu = torch.nn.ReLU()
-        self.l1 = torch.nn.Linear(in_features=in_features,out_features=256)
-        self.l2 = torch.nn.Linear(in_features=256,out_features=128)
-        self.l3 = torch.nn.Linear(in_features=128,out_features=out_features)
-        self.sig = torch.nn.Sigmoid()
-        self.bn1d1 = torch.nn.BatchNorm1d(in_features)
-        self.bn1d2 = torch.nn.BatchNorm1d(256)
-        self.bn1d3 = torch.nn.BatchNorm1d(128)
-
-    def forward(self,x):
-        x = self.bn1d1(x)
-        x = self.relu(self.l1(x))
-        x = self.bn1d2(x)
-        x = self.relu(self.l2(x))
-        x = self.bn1d3(x)
-        x = self.sig(self.l3(x))
-
-        return x
 
 class Model3(torch.nn.Module):
 
